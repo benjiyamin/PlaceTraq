171f43b6da4b9ec374051fd9f1a610fdb58fecd6
Ben Miller <mail@benkmiller.com>
2019-07-07 12:06:49 -0400

    Added some url navigation to views

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,4 +1,5 @@
 module.exports = function (app, passport) {
+  /*
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/',
     failureRedirect: '/signup'
@@ -8,6 +9,18 @@ module.exports = function (app, passport) {
     successRedirect: '/',
     failureRedirect: '/login'
   }))
+  */
+  app.post('/signup', passport.authenticate('local-signup', {
+    failureRedirect: '/signup'
+  }), (request, response) => {
+    response.redirect(`users/${request.user.id}`)
+  })
+
+  app.post('/login', passport.authenticate('local-login', {
+    failureRedirect: '/login'
+  }), (request, response) => {
+    response.redirect(`users/${request.user.id}`)
+  })
 
   app.get('/logout', function (request, response) {
     request.session.destroy(function (error) {
